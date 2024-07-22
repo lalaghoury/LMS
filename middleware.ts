c3b62390922e8ca25909160626d086b46ec70546
lalaghoury <lala.ghoury@gmail.com>
2024-07-22 22:26:51 +0500

    done student block/unblock and assignment grading

--- middleware.ts
+++ middleware.ts
@@ -3,18 +3,24 @@ import type { NextRequest } from "next/server";
 import axios from "axios";
 
 export async function middleware(req: NextRequest, res: NextResponse) {
-  const token = req.cookies.get("auth") || null;
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token?.value}`;
+  const token = req.cookies.get("auth")?.value || null;
+
+  // Set Authorization header if token is present
+  if (token) {
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  }
+
   axios.defaults.withCredentials = true;
   const { pathname } = req.nextUrl;
 
-  const apiPath = pathname.startsWith("/auth")
-    ? "/login"
-    : pathname.startsWith("/batches/enrolled")
-    ? "/enrolled"
-    : pathname.startsWith("/batches/teaching")
-    ? "/teaching"
-    : "/login";
+  // Determine API path based on pathname
+  let apiPath = "/login"; // Default to login path
+
+  if (pathname.startsWith("/batches/enrolled")) {
+    apiPath = "/enrolled";
+  } else if (pathname.startsWith("/batches/teaching")) {
+    apiPath = "/teaching";
+  }
 
   const batchId = pathname.split("/")[3];
 
@@ -35,7 +41,12 @@ export async function middleware(req: NextRequest, res: NextResponse) {
       error.response.data.message ===
       "You are not authorized to perform this action"
     ) {
-      return NextResponse.redirect(new URL(`/batches/${apiPath}`, req.url));
+      return NextResponse.redirect(
+        new URL(
+          `/batches/${apiPath === "/teaching" ? "enrolled" : "teaching"}`,
+          req.url
+        )
+      );
     }
     if (!req.nextUrl.pathname.startsWith("/auth")) {
       return NextResponse.redirect(new URL("/auth/sign-in", req.url));
